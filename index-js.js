cb290f335221c296ef7c9ada7e1f7dc12a1694b1
raopremrao <raom61337@gmail.com>
2026-01-11 16:52:55 +0530

    Adding Connect Functionality

---
+++ index-js.js
@@ -0,0 +1,22 @@
+import {createWalletClient, custom} from "https://esm.sh/viem"
+
+console.log(createWalletClient);
+
+
+const connectButton = document.getElementById("connectButton");
+
+let walletClient;
+
+async function connect() {
+    if(typeof window.ethereum !== "undefined") {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        })
+        await walletClient.requestAddresses();
+        connectButton.innerHTML = "Connected"
+    } else {
+        connectButton.innerHTML = "Please Install MetaMask";
+    }
+}
+
+connectButton.onclick = connect
