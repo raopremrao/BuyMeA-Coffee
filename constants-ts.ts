63d14a7b07560a233383b0e58c5d392d4cb0abaa
raopremrao <raom61337@gmail.com>
2026-01-21 01:01:43 +0530

    Typescript

---
+++ constants-ts.ts
@@ -0,0 +1,87 @@
+export const contractAddress: string =
+  "0x1F59736BC978266bd99858a942D1d94EE8Ff6dCD";
+
+// export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+
+export const abi = [
+  {
+    inputs: [{ internalType: "address", name: "priceFeed", type: "address" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  { inputs: [], name: "FundMe_NotOwner", type: "error" },
+  { stateMutability: "payable", type: "fallback" },
+  {
+    inputs: [],
+    name: "MINIMUM_USD",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "cheaperWithdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fund",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "fundingAddress", type: "address" },
+    ],
+    name: "getAddressToAmountFunded",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "index", type: "uint256" }],
+    name: "getFunder",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getVersion",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "s_addressToAmountFunded",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "s_funders",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  { stateMutability: "payable", type: "receive" },
+] as const;
--- index-js.js
+++ index-js.js
@@ -1,70 +1,73 @@
-import {createWalletClient, custom, createPublicClient, parseEther, defineChain, formatEther} from "https://esm.sh/viem"
+import {
+  createWalletClient,
+  custom,
+  createPublicClient,
+  parseEther,
+  defineChain,
+  formatEther,
+} from "https://esm.sh/viem";
 import { contractAddress, abi } from "./constants-js.js";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
 const ethAmountInput = document.getElementById("ethAmount");
 const balanceButton = document.getElementById("balanceButton");
+const withdrawButton = document.getElementById("withdrawButton");
 
 // console.log(createWalletClient);
-
 
 let walletClient;
 let publicClient;
 
 async function connect() {
-    if(typeof window.ethereum !== "undefined") {
-        walletClient = createWalletClient({
-            transport: custom(window.ethereum)
-        })
-        await walletClient.requestAddresses();
-        connectButton.innerHTML = "Connected"
-    } else {
-        connectButton.innerHTML = "Please Install MetaMask";
-    }
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    });
+    await walletClient.requestAddresses();
+    connectButton.innerHTML = "Connected";
+  } else {
+    connectButton.innerHTML = "Please Install MetaMask";
+  }
 
-    console.log("Hello");
-    
+  console.log("Hello");
 }
 
 async function fund() {
-    const ethAmount = ethAmountInput.value
-    console.log(`Funding with ${ethAmount}...`);
+  const ethAmount = ethAmountInput.value;
+  console.log(`Funding with ${ethAmount}...`);
 
-    if(typeof window.ethereum !== "undefined") {
-        walletClient = createWalletClient({
-            transport: custom(window.ethereum)
-        })
-        const [connectedAccount] = await walletClient.requestAddresses(); // [xyz] is short hand of saying first value(in this case address) of the list
-        const currentChain = await getCurrentChain(walletClient)
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    });
+    const [connectedAccount] = await walletClient.requestAddresses(); // [xyz] is short hand of saying first value(in this case address) of the list
+    const currentChain = await getCurrentChain(walletClient);
 
-        publicClient = createPublicClient({
-            transport: custom(window.ethereum)
-        })
+    publicClient = createPublicClient({
+      transport: custom(window.ethereum),
+    });
 
-        const {request} = await publicClient.simulateContract({
-            address: contractAddress,
-            abi: abi,
-            functionName: "fund",
-            account:connectedAccount,
-            chain: currentChain,
-            value: parseEther(ethAmount), // 1 -> 100000000000000000
-        })
+    const { request } = await publicClient.simulateContract({
+      address: contractAddress,
+      abi: abi,
+      functionName: "fund",
+      account: connectedAccount,
+      chain: currentChain,
+      value: parseEther(ethAmount), // 1 -> 100000000000000000
+    });
 
-        console.log("Done Simulating");
-        
+    console.log("Done Simulating");
 
-        const hash = await walletClient.writeContract(request)
-        console.log(hash);
-                
-
-    } else {
-        connectButton.innerHTML = "Please Install MetaMask";
-    }    
+    const hash = await walletClient.writeContract(request);
+    console.log(hash);
+  } else {
+    connectButton.innerHTML = "Please Install MetaMask";
+  }
 }
 
 async function getCurrentChain(client) {
-  const chainId = await client.getChainId()
+  const chainId = await client.getChainId();
   const currentChain = defineChain({
     id: chainId,
     name: "Custom Chain",
@@ -78,24 +81,59 @@ async function getCurrentChain(client) {
         http: ["http://localhost:8545"],
       },
     },
-  })
-  return currentChain
+  });
+  return currentChain;
 }
 
 async function getBalance() {
-  if(typeof window.ethereum !== "undefined") {
+  if (typeof window.ethereum !== "undefined") {
     publicClient = createPublicClient({
-      transport: custom(window.ethereum)
-    })
-    
+      transport: custom(window.ethereum),
+    });
+
     const balance = await publicClient.getBalance({
-      address: contractAddress
-    })
+      address: contractAddress,
+    });
 
     console.log(formatEther(balance)); // 100000000000000000 -> 1
+  } else {
+    console.log("Please Install MetaMask");
   }
 }
 
-connectButton.onclick = connect
-fundButton.onclick = fund
-balanceButton.onclick = getBalance
+async function withdraw() {
+  console.log(`Withdrawing...`)
+
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      walletClient = createWalletClient({
+        transport: custom(window.ethereum),
+      })
+      publicClient = createPublicClient({
+        transport: custom(window.ethereum),
+      })
+      const [account] = await walletClient.requestAddresses()
+      const currentChain = await getCurrentChain(walletClient)
+
+      console.log("Processing transaction...")
+      const { request } = await publicClient.simulateContract({
+        account,
+        address: contractAddress,
+        abi,
+        functionName: "withdraw",
+        chain: currentChain,
+      })
+      const hash = await walletClient.writeContract(request)
+      console.log("Transaction processed: ", hash)
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    withdrawButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
+withdrawButton.onclick = withdraw;
